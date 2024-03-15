e5ced4b8a77c210bb050c78c0e1e23e7d3c25b70
dooodle74 <dpwang01@gmail.com>
2024-03-15 02:55:45 -0400

    2 columns css

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Head from 'next/head'
 import Header from '@components/Header'
 import Footer from '@components/Footer'
+import Link from 'next/link';
 
 export default function Home() {
   return (
@@ -12,9 +13,24 @@ export default function Home() {
 
       <main>
         <Header title="Welcome to my app!" />
-        <p className="description">
-          body filler 
-        </p>
+
+        <div className="columns"> 
+          {/* First Column */}
+          <div className="column"> 
+            <p className="description">Short description for image 1</p> 
+            <Link href="/photography">
+              <button className="button">Button 1</button> 
+            </Link>
+          </div>
+
+          {/* Second Column */}
+          <div className="column"> 
+            <p className="description">Short description for image 2</p> 
+            <Link href="/projects">
+              <button className="button">Button 2</button> 
+            </Link>
+          </div>
+        </div>
       </main>
 
       <Footer />
