b1fbd63698916de2dcb11fdc59d06c66b1233849
dooodle74 <dpwang01@gmail.com>
2024-03-15 03:08:36 -0400

    try new css

--- pages/index.js
+++ pages/index.js
@@ -14,8 +14,19 @@ export default function Home() {
       <main>
         <Header title="Welcome to my app!" />
 
+        <div className="menu">
+          <p className="description">Short description for image 1</p> 
+            <Link href="/photography">
+              <button className="button">Button 1</button> 
+            </Link>
+
+            <p className="description">Short description for image 2</p> 
+            <Link href="/projects">
+              <button className="button">Button 2</button> 
+            </Link>
+        </div>
+
         <div className="column"> 
-          {/* First Column */}
           <div className="column"> 
             <p className="description">Short description for image 1</p> 
             <Link href="/photography">
@@ -23,7 +34,6 @@ export default function Home() {
             </Link>
           </div>
 
-          {/* Second Column */}
           <div className="column"> 
             <p className="description">Short description for image 2</p> 
             <Link href="/projects">
