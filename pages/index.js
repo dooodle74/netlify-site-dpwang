c8669af33e83ad3d4a40ee717c1ce8c19866953e
dooodle74 <dpwang01@gmail.com>
2024-03-15 05:07:08 -0400

    div clickable

--- pages/index.js
+++ pages/index.js
@@ -16,24 +16,23 @@ export default function Home() {
 
         <div className="columns"> 
           {/* First Column */}
-          <div className="column"> 
-            <img src="/icon_code.png" alt="Simple icon of code bracket" className="image-centered" />
-            <p className="subtitle">Projects</p> 
-            <p className="description">i code</p> 
-            <Link href="/photography">
-              <button className="button">See Projects</button> 
-            </Link>
-          </div>
+          <Link href="/projects">
+            <div className="column"> 
+              <img src="/icon_code.png" alt="Simple icon of code bracket" className="image-centered" />
+              <p className="subtitle">Projects</p> 
+              <p className="description">i code</p> 
+            </div>
+          </Link>
 
           {/* Second Column */}
-          <div className="column"> 
-            <img src="/icon_camera.png" alt="Simple icon of camera" className="image-centered" />
-            <p className="subtitle">Photography</p> 
-            <p className="description">i take photos</p> 
-            <Link href="/projects">
-              <button className="button">See Gallery</button> 
-            </Link>
-          </div>
+          <Link href="/photography">
+            <div className="column"> 
+              <img src="/icon_camera.png" alt="Simple icon of camera" className="image-centered" />
+              <p className="subtitle">Photography</p> 
+              <p className="description">i take photos</p> 
+            </div>
+          </Link>
+          
         </div>
       </main>
 
