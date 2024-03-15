174065055291ec4df901fb4720678c71b53a5bb1
dooodle74 <dpwang01@gmail.com>
2024-03-15 03:37:34 -0400

    revert

--- pages/index.js
+++ pages/index.js
@@ -14,30 +14,23 @@ export default function Home() {
       <main>
         <Header title="Welcome to my app!" />
 
-
-        <div class="menu">
-          <div class="item">Div 1</div>
-          <div class="item">Div 2</div>
-          <div class="item">Div 3</div>
-          <div class="item">Div 4</div>
-        </div>
-
-        <div className="container">
-          <div>
+        <div className="column"> 
+          {/* First Column */}
+          <div className="column"> 
             <p className="description">Short description for image 1</p> 
-              <Link href="/photography">
-                <button className="button">Button 1</button> 
-              </Link>
+            <Link href="/photography">
+              <button className="button">Button 1</button> 
+            </Link>
           </div>
 
-          <div>
+          {/* Second Column */}
+          <div className="column"> 
             <p className="description">Short description for image 2</p> 
-              <Link href="/projects">
-                <button className="button">Button 2</button> 
-              </Link>
+            <Link href="/projects">
+              <button className="button">Button 2</button> 
+            </Link>
           </div>
         </div>
-
       </main>
 
       <Footer />
