ba5c6ea1dd516f14635784579552068e13e43f45
dooodle74 <dpwang01@gmail.com>
2024-03-15 05:31:55 -0400

    ???

--- pages/index.js
+++ pages/index.js
@@ -16,23 +16,24 @@ export default function Home() {
 
         <div className="columns"> 
           {/* First Column */}
-          <Link href="/projects">
-            <div className="column"> 
-              <img src="/icon_code.png" alt="Simple icon of code bracket" className="image-centered" />
-              <p className="subtitle">Projects</p> 
-              <p className="description">i code</p> 
-            </div>
-          </Link>
+          <div className="column"> 
+            <img src="/icon_code.png" alt="Simple icon of code bracket" className="image-centered" />
+            <p className="subtitle">Projects</p> 
+            <p className="description">i code "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p> 
+            <Link href="/photography">
+              <button className="button">See Projects</button> 
+            </Link>
+          </div>
 
           {/* Second Column */}
-          <Link href="/photography">
-            <div className="column"> 
-              <img src="/icon_camera.png" alt="Simple icon of camera" className="image-centered" />
-              <p className="subtitle">Photography</p> 
-              <p className="description">i take photos</p> 
-            </div>
-          </Link>
-          
+          <div className="column"> 
+            <img src="/icon_camera.png" alt="Simple icon of camera" className="image-centered" />
+            <p className="subtitle">Photography</p> 
+            <p className="description">i take photos "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p> 
+            <Link href="/projects">
+              <button className="button">See Gallery</button> 
+            </Link>
+          </div>
         </div>
       </main>
 
