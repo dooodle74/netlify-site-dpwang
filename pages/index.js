0fd33f7bc6cec8b89716ac4706f9eaf50712d07d
dooodle74 <dpwang01@gmail.com>
2024-03-16 02:52:41 -0400

    wrong buttons homepage, change API fetching

--- pages/index.js
+++ pages/index.js
@@ -20,7 +20,7 @@ export default function Home() {
             <img src="/icon_code.png" alt="Simple icon of code bracket" className="image-centered" />
             <p className="subtitle">Projects</p> 
             <p className="description">i code "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p> 
-            <Link href="/photography">
+            <Link href="/projects">
               <button className="button">See Projects</button> 
             </Link>
           </div>
@@ -30,7 +30,7 @@ export default function Home() {
             <img src="/icon_camera.png" alt="Simple icon of camera" className="image-centered" />
             <p className="subtitle">Photography</p> 
             <p className="description">i take photos "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p> 
-            <Link href="/projects">
+            <Link href="/photography">
               <button className="button">See Gallery</button> 
             </Link>
           </div>
