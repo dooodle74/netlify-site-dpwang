4d04bf0c11b634ed7a411c359ddfbc4642e5c10b
dooodle74 <dpwang01@gmail.com>
2024-03-18 01:42:10 -0400

    fix

--- pages/dailypi.js
+++ pages/dailypi.js
@@ -1,15 +1,7 @@
-import Head from 'next/head'
-import Header from '@components/Header'
-import Footer from '@components/Footer'
-import Link from 'next/link';
 import { useState, useEffect } from 'react';
 
 export default function PiDigits() {
   const [digits, setDigits] = useState('');
-  const startTime = 1708905600000;
-  const startDigit = 101;
-  const numDigits = 4;
-  const msDay = 86400000;
 
   useEffect(() => {
     // Fetch the digits from 'data.txt'
@@ -26,47 +18,23 @@ export default function PiDigits() {
     fetchData();
   }, []);
 
-  function getEpochTime() {
-    const timestamp = Date.now();
-    return timestamp;
+  function formatDigits(digits) {
+    let digitsFormatted = '3.\n';
+
+    for (let line = 0; line < digits.length; line += 40) {
+      for (let group = line; group < line + 40; group += 4) {
+        digitsFormatted += digits.substring(group, group + 4);
+        digitsFormatted += ' ';
+      }
+      digitsFormatted = digitsFormatted + ' . (' + (line + 40).toString() + ')';
+      digitsFormatted += '\n';
+    }
+    return digitsFormatted;
   }
-
-  const epochDelta = getEpochTime() - startTime;
-  const pastDays = Math.floor(epochDelta / msDay);
-  const currentStartDigit = startDigit + pastDays * numDigits;
-  const currentEndDigit = currentStartDigit + numDigits;
-
-  function getSubstring(offset) {
-    const digitOffset = offset * numDigits;
-    const startIndex = currentStartDigit + digitOffset;
-    const endIndex = currentEndDigit + digitOffset;
-
-    return digits.substring(startIndex, endIndex);
-  }
-
-  const someDigit = getSubstring(-1);
-
-  const renderDigits = () => {
-    const digitsArray = Array.from({ length: 7 }, (_, i) => i - 3);
-    return digitsArray.map(offset => {
-      const stringSlice = getSubstring(offset);
-      const elementId = `digit${offset}`;
-      return <p className={offset === 0 ? 'focus' : 'regular'} key={elementId} id={elementId}>{stringSlice}</p>;
-    });
-  };
-
-  const infoDay = (pastDays + 1).toString();
-  const infoDigits = (currentEndDigit - 1).toString();
-  const infoFull = `${infoDay}\n${infoDigits}`;
-
-  const handleClick = () => {
-    window.location.href = '/pi10000';
-  };
 
   return (
     <div>
-      {renderDigits()}
-      <p className="info" id="counter" onClick={handleClick}>{infoFull}</p>
+      <p id="digits" dangerouslySetInnerHTML={{ __html: formatDigits(digits) }}></p>
     </div>
   );
 }
