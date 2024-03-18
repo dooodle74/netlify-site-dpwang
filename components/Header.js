3acd590af80aa1b4321ad28884864e10896d9d3a
dooodle74 <dpwang01@gmail.com>
2024-03-18 16:12:43 -0400

    better header

--- components/Header.js
+++ components/Header.js
@@ -1,14 +1,23 @@
-export default function Header({ title }) {
+// Header.js
+import React from 'react';
+
+const Header = () => {
   return (
-    <header>
-      <h1 className="title">{title}</h1>
-      <nav>
-        <ul>
-          <li><a href="/">Home</a></li>
-          <li><a href="/projects">Projects</a></li>
-          <li><a href="/photography">Photography</a></li>
-        </ul>
-      </nav>
+    <header className="fixed-top">
+      <div className="container">
+        <div className="left-section">
+          <a href="/" className="btn btn-primary">Home</a>
+          <a href="/projects" className="btn btn-primary">Projects</a>
+          <a href="/photography" className="btn btn-primary">Photography</a>
+        </div>
+        <div className="right-section">
+          <a href="https://linkedin.com" className="btn btn-primary">LinkedIn</a>
+          <a href="https://github.com" className="btn btn-primary">GitHub</a>
+          <a href="mailto:dpwang01@gmail.com" className="btn btn-primary">Contact</a>
+        </div>
+      </div>
     </header>
   );
 }
+
+export default Header;
