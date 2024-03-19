fe1b16b1a2dd2f58ee124f296a314f3d03c1c7ae
dooodle74 <dpwang01@gmail.com>
2024-03-19 02:09:22 -0400

    test header

--- components/Header.js
+++ components/Header.js
@@ -4,18 +4,7 @@ import styles from '/styles/Header.module.css';
 
 const Header = () => {
   return(
-    <div className = {styles.body}>
-      <div className = {styles.ul}>
-        <div className = {styles.li}><a href="/">Home</a></div>
-        <div class={styles.dropdown}>
-          <a href="javascript:void(0)" class={styles.dropbtn}>Dropdown</a>
-          <div class={styles.dropdown-content}>
-            <a href="/pages/projects">Projects</a>
-            <a href="/pages/photography">Photography</a>
-          </div>
-        </div>
-        </div>
-      </div>
+    <p>hello</p>
   );
 }
 
