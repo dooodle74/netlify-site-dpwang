51785dd98547adfff340930c24765c8128055e4d
dooodle74 <dpwang01@gmail.com>
2024-03-18 21:40:47 -0400

    fixed styles calling

--- components/Header.js
+++ components/Header.js
@@ -4,18 +4,18 @@ import styles from '/styles/Header.module.css';
 
 const Header = () => {
   return(
-    <body>
-      <ul>
-        <li><a href="/">Home</a></li>
-        <li class={styles.dropdown}>
+    <div className = {styles.body}>
+      <div className = {styles.ul}>
+        <div className = {styles.li}><a href="/">Home</a></div>
+        <div class={styles.dropdown}>
           <a href="javascript:void(0)" class={styles.dropbtn}>Dropdown</a>
           <div class={styles.dropdown-content}>
             <a href="/projects">Projects</a>
             <a href="/photography">Photography</a>
           </div>
-        </li>
-      </ul>
-      </body>
+        </div>
+        </div>
+      </div>
   );
 }
 
