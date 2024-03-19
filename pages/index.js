8110cbcf380cdb7c6e1c8d3395c2a193f2a55e5e
dooodle74 <dpwang01@gmail.com>
2024-03-19 18:31:01 -0400

    social links homepage

--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,25 @@ export default function Home() {
 
       <main>
         <Header/>
-        <h1>Hello!</h1>
+        <h1>Welcome to David's Homepage!</h1>
+        <div className="columns">
+          <div className="column">
+            <Link href="https://www.linkedin.com/in/dongpingwang/">
+              <img src="/logo_LinkedIn.png" alt="LinkedIn Pofile" className="image-centered" />
+            </Link>
+          </div>
+          <div className="column">
+            <Link href="https://github.com/dooodle74">
+              <img src="/logo_Github.png" alt="Github Pofile" className="image-centered" />
+            </Link>
+          </div>
+          <div className="column">
+            <Link href="mailto:dpwang01@gmail.com">
+              <img src="/logo_email.png" alt="Send email to dpwang01@gmail.com" className="image-centered" />
+            </Link>
+          </div>
+        </div>
+
         <div className="columns"> 
           {/* First Column */}
           <div className="column"> 
