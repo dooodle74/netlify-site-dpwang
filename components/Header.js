1cc77c9a806c92989adce786c01b546f7c627b44
dooodle74 <dpwang01@gmail.com>
2024-03-20 21:19:35 -0400

    header, center vertical

--- components/Header.js
+++ components/Header.js
@@ -1,8 +1,10 @@
+import styles from './Header.module.css';
+
 export default function Header() {
   return (
     <header>
     <nav>
-    <ul class="nav">
+    <ul className={styles.ul}>
       <li><a href="/">Home</a></li>
     </ul>
     </nav>
