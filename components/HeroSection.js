8419c2e0a7f9df214e4509b9aa90d1f248e3aff0
dooodle74 <dpwang01@gmail.com>
2024-03-20 20:37:37 -0400

    changed to div from section

--- components/HeroSection.js
+++ components/HeroSection.js
@@ -3,12 +3,12 @@ import styles from './HeroSection.module.css'
 
 const HeroSection = () => {
   return (
-    <section className={styles.hero-section}>
+    <div className={styles.hero-section}>
       <div className={styles.hero-content}>
         <h1>Welcome to My Website</h1>
         <p>A brief description or tagline can go here.</p>
       </div>
-    </section>
+    </div>
   );
 };
 
