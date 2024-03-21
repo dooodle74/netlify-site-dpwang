5346ee8e55288f908f54cb1a9f6464825a68a737
dooodle74 <dpwang01@gmail.com>
2024-03-20 22:05:20 -0400

    font size agian

--- components/HeroSection.js
+++ components/HeroSection.js
@@ -5,7 +5,7 @@ const HeroSection = () => {
   return (
     <section className={styles.section}>
       <div className={styles.content}>
-        <p>Hi, I am</p>
+        <p className={styles.pretitle}>HELLO, I AM</p>
         <h1 className={styles.title}>David Wang</h1>
         <h2 className={styles.subtitle}>Welcome to my homepage.</h2>
       </div>
