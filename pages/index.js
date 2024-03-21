38556192339adab514aaaa94e4da6a4752e9078e
dooodle74 <dpwang01@gmail.com>
2024-03-20 20:19:18 -0400

    wrong folder lmfao

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import Head from 'next/head'
 import Header from '@components/Header'
+import HeroSection from '@components/HeroSection';
 import Footer from '@components/Footer'
 import Link from 'next/link';
 
@@ -12,53 +13,7 @@ export default function Home() {
       </Head>
 
       <main>
-        <Header/>
-        <div className="fullWidthSection">
-          <h1>Welcome to David's Homepage!</h1>
-        </div>
-        <div className="fullWidthSection">
-          <div className="smallWidthSection">
-            <div className="columns">
-              <div className="column-logo">
-                <Link href="https://www.linkedin.com/in/dongpingwang/">
-                  <img src="/logo_LinkedIn.png" alt="LinkedIn Pofile" className="logo-centered" />
-                </Link>
-              </div>
-              <div className="column-logo">
-              < Link href="https://github.com/dooodle74">
-                  <img src="/logo_Github.png" alt="Github Pofile" className="logo-centered" />
-                </Link>
-              </div>
-              <div className="column-logo">
-                <Link href="mailto:dpwang01@gmail.com">
-                  <img src="/logo_email.png" alt="Send email to dpwang01@gmail.com" className="logo-centered" />
-                </Link>
-              </div>
-            </div>
-          </div>
-        </div>
-
-        <div className="columns"> 
-          {/* First Column */}
-          <div className="column"> 
-            <img src="/icon_code.png" alt="Simple icon of code bracket" className="image-centered" />
-            <p className="subtitle">Projects</p> 
-            <p className="description">Developer. See projects.</p> 
-            <Link href="/projects">
-              <button className="button">See Projects</button> 
-            </Link>
-          </div>
-
-          {/* Second Column */}
-          <div className="column"> 
-            <img src="/icon_camera.png" alt="Simple icon of camera" className="image-centered" />
-            <p className="subtitle">Photography</p> 
-            <p className="description">Amature photographer. See my photos</p> 
-            <Link href="/photography">
-              <button className="button">See Gallery</button> 
-            </Link>
-          </div>
-        </div>
+        <HeroSection />
       </main>
 
       <Footer />
