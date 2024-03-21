3f8e5d4f6dd37573d7fa9fc5233bad1e5c538a01
dooodle74 <dpwang01@gmail.com>
2024-03-21 00:48:04 -0400

    fix flex

--- components/AboutMeSection.js
+++ components/AboutMeSection.js
@@ -4,9 +4,7 @@ import styles from './AboutMeSection.module.css';
 const AboutMeSection = () => {
   return (
     <section id="about" className={styles.section}>
-      <div className={styles.title}>
-        <h1>About Me</h1>
-      </div>
+      <div className={styles.title}>About Me</div>
       <div className={styles.content}>
         Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
       </div>
