2cfaf83329eaa732c1c02e6c4724c0dd0e302380
dooodle74 <dpwang01@gmail.com>
2024-03-21 20:10:01 -0400

    change to individual padding

--- pages/index.js
+++ pages/index.js
@@ -15,12 +15,10 @@ export default function Home() {
       <main>
         <Header />
         <HeroSection />
-        <body>
-          <AboutMeSection />
-          <AboutMeSection />
-          <AboutMeSection />
-          <AboutMeSection />
-        </body>
+        <AboutMeSection />
+        <AboutMeSection />
+        <AboutMeSection />
+        <AboutMeSection />
         <Footer />
       </main>
     </div>
