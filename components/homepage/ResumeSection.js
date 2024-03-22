dccd74bc4c7abe46b2ca8c563d36dd5d1024548b
dooodle74 <dpwang01@gmail.com>
2024-03-22 13:55:14 -0400

    test

--- components/homepage/ResumeSection.js
+++ components/homepage/ResumeSection.js
@@ -7,9 +7,9 @@ const ResumeSection = () => {
     <section id="experience" className={styles.section}>
         <h1 className={styles.title}>Experience</h1>
         <div className={styles.subsection}>
-            <h2 className={styles.subsection-title}>Software Engineer Intern, <Link href='https://ev.buaa.edu.cn/'><a>Beihang University</a></Link></h2>
-            <h3 className={styles.subsection-subtitle}>June 2023 - July 2023</h3>
-            <div className={styles.subsection-body}>
+            <h2 className={styles.subsectionTitle}>Software Engineer Intern, <Link href='https://ev.buaa.edu.cn/'><a>Beihang University</a></Link></h2>
+            <h3 className={styles.subsectionSubtitle}>June 2023 - July 2023</h3>
+            <div className={styles.subsectionBody}>
                 Interned for a simulator team at the university. Implemented optimization solutions that determine effective loading instructions for cargo aircraft, which increased efficiency by 40% while maintaining accuracy. 
             </div>
         </div>
