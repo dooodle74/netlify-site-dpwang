39c3d7d9d70ad76422228602eff11071ad63bb57
dooodle74 <dpwang01@gmail.com>
2024-03-23 03:48:46 -0400

    fix footer

--- components/Footer.js
+++ components/Footer.js
@@ -5,8 +5,8 @@ import styles from '/styles/Footer.module.css';
 const Footer = () => {
   return(
     <footer className={styles.footer}>
-      © Dongping Wang 2024. Powered by Netlify.<br/>
-      <Link href="https://github.com/dooodle74/netlify-site-dpwang/" target="_blank" rel="noopener noreferrer"><a>View Source</a></Link> | <Link href="mailto:dpwang01@gmail.com"><a>Contact</a></Link>
+      <div>© Dongping Wang 2024. Powered by Netlify.</div>
+      <div><Link href="https://github.com/dooodle74/netlify-site-dpwang/" target="_blank" rel="noopener noreferrer"><a>View Source</a></Link> | <Link href="mailto:dpwang01@gmail.com"><a>Contact</a></Link></div>
     </footer>
   );
 }
