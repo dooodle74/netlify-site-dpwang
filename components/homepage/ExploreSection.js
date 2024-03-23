c0f39a517db048700f64d483b1229c5255d4ec25
dooodle74 <dpwang01@gmail.com>
2024-03-23 04:23:56 -0400

    test tiles section

---
+++ components/homepage/ExploreSection.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import Link from 'next/link';
+import styles from '/styles/homepage/Explore.module.css';
+
+const Explore = () => {
+  return(
+    <section id="more" className={styles.section}>
+        <div className={styles.title}>More</div>
+        <div className={styles.container}>
+            <div class={styles.tile} onclick="location.href='/projects'">
+                <h2>Projects</h2>
+                <p>Description of Tile 1</p>
+            </div>
+
+            <div class={styles.tile} onclick="location.href='/photography'">
+                <h2>Photography</h2>
+                <p>Description of Tile 2</p>
+            </div>
+        </div>
+    </section>
+  );
+}
+
+export default Explore;
