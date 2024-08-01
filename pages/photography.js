2a06c9d176ca8bf5ded05f2896a1a1e401edc411
dooodle74 <dpwang01@gmail.com>
2024-08-01 16:53:33 -0400

    photo margins

--- pages/photography.js
+++ pages/photography.js
@@ -100,9 +100,6 @@ export default function Photography() {
             ))}
           </nav>
         )}
-        <div style="width: 20px;">
-
-        </div>
         <Footer />
       </main>
     </div>
