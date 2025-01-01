568d12c525403761c5c741ddbf45a48d0db760fa
dooodle74 <dpwang01@gmail.com>
2024-12-31 23:58:45 -0500

    attempts to make resume clicable

--- components/homepage/ResumeSection.js
+++ components/homepage/ResumeSection.js
@@ -5,18 +5,25 @@ import Link from 'next/link';
 const ResumeSection = () => {
   return (
       <section id="resume" className="sectionGray">
-        <div className='container container800'>
+        <div className='container'>
+          {/*  container800 */}
             <div className={styles.title}>Education</div>
 
-            <div className={styles.subsection}>
-                <div className={styles.subsectionTitle}>Bachelor's in Computer Science and Mathematics, <Link href="https://www.virginia.edu/" target="_blank" rel="noopener noreferrer">University of Virginia </Link></div>
-                <div className={styles.subsectionSubtitle}>August 2022 - Present</div>
-                <div className={styles.subsectionText}>
-                    Expected graduation: <i>June 2025.</i><br />
-                    3.8 GPA, Dean's List.<br />
-                    Relevant Coursework: <i>Data Structures and Algorithms, Computer Systems and Organization, Software Engineering, Discrete Math, Theory of Computation, Abstract Algebra, Real Analysis, Stochastic Processes, Mathemetical Statistics.</i>
+            <Link className={styles.aLinkNoShow} href="https://www.virginia.edu/" target="_blank" rel="noopener noreferrer" legacyBehavior>
+            <a className={styles.subsection}>
+                <div className={styles.subsectionTitle}>Bachelor's Degree, <Link href="https://www.virginia.edu/" target="_blank" rel="noopener noreferrer">University of Virginia </Link></div>
+                <div className={styles.subsectionSubtitle}>
+                    August 2022 - Present
                 </div>
-            </div>
+                <div className={styles.subsectionText}>
+                    Double Major in Computer Science and Mathematics (Probability and Statistics Concentration)<br/>
+                    3.8 GPA, Dean's List.<br />
+                    <ul>
+                        <li>Relevant Coursework: <i>Data Structures and Algorithms, Computer Systems, Software Engineering, AI, Machine Learning, Discrete Mathematics, Abstract Algebra, Real Analysis, Probability, Stochastic Processes, Statistics.</i></li>
+                    </ul>
+                </div>
+            </a>
+            </Link>
             
             <div className="divider" />
 
@@ -26,7 +33,11 @@ const ResumeSection = () => {
                 <div className={styles.subsectionTitle}>Researcher, <Link href="https://engineering.virginia.edu/labs-groups/infrastructure-simulation-sensing-and-evaluation-lab" target="_blank" rel="noopener noreferrer">IS2EE Lab</Link>, University of Virginia Department of Engineering</div>
                 <div className={styles.subsectionSubtitle}>May 2024 - Present</div>
                 <div className={styles.subsectionText}>
-                Implemented, developed, and tested Convolutional Neural Network (CNN) models. Contributed to a larger project aimed at developing an application for detecting structural integrity in civil engineering settings. Collaborated with a multidisciplinary team to ensure the models meet the practical needs of civil engineering applications.
+                    <ul>
+                        <li>Generated 25,000+ sample speckle image pairs using simulation algorithms and lab photography.</li>
+                        <li>Built, trained, and tuned CNN models for displacement measurement, resulting in 0.005-pixel validation MSE.</li>
+                        <li>Contributed to practical advancements in displacement prediction technology for civil engineering applications.</li>
+                    </ul>
                 </div>
             </div>
 
@@ -36,26 +47,36 @@ const ResumeSection = () => {
 
             <div className={styles.subsection}>
                 <div className={styles.subsectionTitle}>Teaching Assistant, <Link href="https://engineering.virginia.edu/department/computer-science" target="_blank" rel="noopener noreferrer">Department of Computer Science</Link>, University of Virginia</div>
-                <div className={styles.subsectionSubtitle}>August 2024 - Present</div>
+                <div className={styles.subsectionSubtitle}>August 2024 - May 2025</div>
                 <div className={styles.subsectionSubtitle}>Courses Taught: CS 3240 "Software Engineering"</div>
                 <div className={styles.subsectionText}>
-                    Supervised group project development and offered feedback on design and progress and held weekly office hours to assist students and provided grading for assignments and exams.
+                    <ul>
+                        <li>Guided class project development, grading, and held office hours for a class of 300 students.</li>
+                        <li>Project manager for two project groups: conducted six sprint checks in an agile development context.</li>
+                        <li>Utilized Django, Google Cloud, Git, and AWS S3 for coursework and collaboration improvements.</li>
+                    </ul>
                 </div>
             </div>
 
             <div className={styles.subsection}>
-                <div className={styles.subsectionTitle}>AI Model Trainer, <Link href="https://www.linkedin.com/company/dataannotationtech/" target="_blank" rel="noopener noreferrer">DataAnnotation</Link></div>
+                <div className={styles.subsectionTitle}>AI Trainer and Software Developer, <Link href="https://www.linkedin.com/company/dataannotationtech/" target="_blank" rel="noopener noreferrer">DataAnnotation</Link></div>
                 <div className={styles.subsectionSubtitle}>April 2024 - Present</div>
                 <div className={styles.subsectionText}>
-                    Performed high-quality data annotation for machine learning projects, including implementing programming solutions, image labeling, object detection, and text classification. Conducted rigorous testing of current AI models and those in production to evaluate performance and identify areas for improvement.
+                    <ul>
+                        <li>Annotated data for 10+ AI projects, produced high-quality code responses, and led quality assurance efforts.</li>
+                        <li>Developed, tested, and fine-tuned chatbot prompts across coding, advanced mathematics, and Mandarin language topics, analyzed against outgoing models (e.g. GPT-4), and ensured high-quality solutions.</li>
+                    </ul>
                 </div>
             </div>
 
             <div className={styles.subsection}>
-                <div className={styles.subsectionTitle}>Software Engineer Intern, Haidian Systems Engineering and Application Technology R&D Team</div>
+                <div className={styles.subsectionTitle}>Software Engineer Intern, <Link href="https://is.buaa.edu.cn/en/" target="_blank" rel="noopener noreferrer">Beihang University</Link></div>
                 <div className={styles.subsectionSubtitle}>June 2023 - August 2023</div>
                 <div className={styles.subsectionText}>
-                    Interned for a systems engineering and AR simulator R&D team in Beijing's university district. I contributed to the design and implementation of optimization solutions that determine effective loading patterns for cargo aircraft, which increased efficiency by 40% while maintaining a high-level of space efficiency. 
+                    <ul>
+                        <li>Implemented algorithms to solve NP-hard problems for cargo aircraft loading, utilizing dynamic programming.</li>
+                        <li>Help integrate algorithms into a simulation engine in a production environment, increasing efficiency by 40%</li>
+                    </ul>
                 </div>
             </div>
 
